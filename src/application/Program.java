6a2ec9b51ec39372bdfc37dccc8befaefcbd27f3
elvismaicol <elvismaicol@gmail.com>
2020-02-08 21:27:59 -0300

    Exercicio 02 de Lambda

---
+++ src/application/Program.java
@@ -0,0 +1,29 @@
+/*
+ * Fazer um programa para ler os dados (nome, email e salário)
+de funcionários a partir de um arquivo em formato .csv.
+    Em seguida mostrar, em ordem alfabética, o email dos
+funcionários cujo salário seja superior a um dado valor
+fornecido pelo usuário. 
+    Mostrar também a soma dos salários dos funcionários cujo
+nome começa com a letra 'M'.
+ */
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Elvis
+ */
+public class Program {
+
+    public static void main(String[] args) {
+        
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        
+        
+    }
+    
+}
