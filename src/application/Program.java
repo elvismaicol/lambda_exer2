09cd38bdbf9d4e581bf42b3ebc290cfaee5e985a
elvismaicol <elvismaicol@gmail.com>
2020-02-08 22:38:23 -0300

    Exercicio 02 de Lambda - Ordem alfabetica

--- src/application/Program.java
+++ src/application/Program.java
@@ -9,8 +9,16 @@ nome começa com a letra 'M'.
  */
 package application;
 
+import entities.Employee;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 /**
  *
@@ -23,7 +31,34 @@ public class Program {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
         
+        System.out.println("Enter full file path: ");
+        String path = sc.nextLine();
         
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+            List<Employee> list = new ArrayList<>();
+            
+            String line = br.readLine();
+            while (line != null) {
+                String[] fields = line.split(",");
+                list.add(new Employee(fields[0], fields[1], Double.parseDouble(fields[2])));
+                line = br.readLine();
+            }
+            System.out.println("Enter salary: ");
+            Double sal = sc.nextDouble();
+            
+            Comparator<String> comp = (s1,s2) -> s1.toUpperCase().compareTo(s2.toUpperCase());
+            
+            List<String> nomes = list.stream()
+                    .filter(p -> p.getSalary() > sal)
+                    .map(p -> p.getEmail())
+                    .sorted(comp)
+                    .collect(Collectors.toList());
+            
+            nomes.forEach(System.out :: println);
+            
+        }catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+        sc.close();
     }
-    
 }
