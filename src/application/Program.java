35d1cb835b800da3e4f4ed9273827e6653b6af55
elvismaicol <elvismaicol@gmail.com>
2020-02-08 23:14:57 -0300

    Exercicio 02 de Lambda - Soma

--- src/application/Program.java
+++ src/application/Program.java
@@ -14,7 +14,6 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
@@ -46,15 +45,22 @@ public class Program {
             System.out.println("Enter salary: ");
             Double sal = sc.nextDouble();
             
-            Comparator<String> comp = (s1,s2) -> s1.toUpperCase().compareTo(s2.toUpperCase());
+            System.out.println("Email of people whose salary is more than " + String.format("%.2f", sal) + ":");
             
-            List<String> nomes = list.stream()
-                    .filter(p -> p.getSalary() > sal)
-                    .map(p -> p.getEmail())
-                    .sorted(comp)
+            List<String> emails = list.stream()
+                    .filter(e -> e.getSalary() > sal)
+                    .map(e -> e.getEmail())
+                    .sorted()
                     .collect(Collectors.toList());
             
-            nomes.forEach(System.out :: println);
+            emails.forEach(System.out :: println);
+            
+            double sum = list.stream()
+                    .filter(e -> e.getName().charAt(0) == 'M')
+                    .map(e -> e.getSalary())
+                    .reduce(0.0, (x,y) -> x + y);
+            
+            System.out.print("Sum of salary of people whose name starts with 'M': " + String.format("%.2f", sum));
             
         }catch (IOException e) {
             System.out.println("Error: " + e.getMessage());
